e1f709f11e26c176c2d4153220f131a05d6c5a9b
Fabian Wrede <fabian.wrede@wi.uni-muenster.de>
2018-05-02 16:22:58 +0200

    some tests with OpenMP declare reduction

--- reference-implementations/sum-structs/CPU/build-and-run.sh
+++ reference-implementations/sum-structs/CPU/build-and-run.sh
@@ -9,4 +9,6 @@ cd build/ && \
 cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Dev ../ && \
 
 make mpi && \
-mpirun -np 4 bin/mpi 
+make openmp && \
+mpirun -np 4 bin/mpi  && \
+mpirun -np 1 bin/openmp
