08d5b95fa5c5dd95f772fc5d4db9d88640c3218d
Fabian Wrede <fabian.wrede@wi.uni-muenster.de>
2018-06-12 11:44:34 +0200

    re-gen for testing

--- src-gen/hlpp18/nbody_float-n-4-c-24/CPU-MPMD/job.sh
+++ src-gen/hlpp18/nbody_float-n-4-c-24/CPU-MPMD/job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name nbody_float-n-4-c-24-release-nodes-4-cpu-24
+#SBATCH --job-name nbody_float-n-4-c-24-CPU-MPMD-nodes-4-cpu-24
 #SBATCH --ntasks 4
 #SBATCH --nodes 4
 #SBATCH --ntasks-per-node 1
 #SBATCH --partition haswell
 #SBATCH --exclude taurusi[1001-1270],taurusi[3001-3180],taurusi[2001-2108],taurussmp[1-7],taurusknl[1-32]
-#SBATCH --output /home/fwrede/musket-build/hlpp18/nbody_float-n-4-c-24/CPU-MPMD/out/nbody_float-n-4-c-24-release-nodes-4-cpu-24.out
+#SBATCH --output /home/fwrede/musket-build/hlpp18/nbody_float-n-4-c-24/CPU-MPMD/out/nbody_float-n-4-c-24-nodes-4-cpu-24.out
 #SBATCH --cpus-per-task 24
 #SBATCH --mail-type ALL
 #SBATCH --mail-user fabian.wrede@mailbox.tu-dresden.de
@@ -16,5 +16,5 @@ export OMP_NUM_THREADS=24
 
 RUNS=10
 for ((i=1;i<=RUNS;i++)); do
-    srun  /home/fwrede/musket-build/hlpp18/nbody_float-n-4-c-24/CPU-MPMD/build/benchmark/bin/nbody_float-n-4-c-24_0 : /home/fwrede/musket-build/hlpp18/nbody_float-n-4-c-24/CPU-MPMD/build/benchmark/bin/nbody_float-n-4-c-24_1 : /home/fwrede/musket-build/hlpp18/nbody_float-n-4-c-24/CPU-MPMD/build/benchmark/bin/nbody_float-n-4-c-24_2 : /home/fwrede/musket-build/hlpp18/nbody_float-n-4-c-24/CPU-MPMD/build/benchmark/bin/nbody_float-n-4-c-24_3
+    srun --multi-prog /home/fwrede/musket/hlpp18/nbody_float-n-4-c-24/CPU-MPMD/job.conf
 done	
