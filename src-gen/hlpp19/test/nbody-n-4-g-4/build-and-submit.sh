332ed2fe0fb2cb71cbec826ede0e3e4f08617330
Fabian Wrede <fabian.wrede@wi.uni-muenster.de>
2019-03-29 12:39:52 +0100

    nbody test

---
+++ src-gen/hlpp19/test/nbody-n-4-g-4/build-and-submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source_folder=${PWD} && \
+
+# remove files and create folder
+mkdir -p /home/fwrede/out/hlpp19/musket-test/nbody-n-4-g-4/out && \
+rm -rf -- /home/fwrede/build/hlpp19/musket/nbody-n-4-g-4 && \
+mkdir -p /home/fwrede/build/hlpp19/musket/nbody-n-4-g-4 && \
+
+# run cmake
+cd /home/fwrede/build/hlpp19/musket/nbody-n-4-g-4 && \
+cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Benchmarktaurus ${source_folder} && \
+
+make nbody-n-4-g-4_0 && \
+make nbody-n-4-g-4_1 && \
+make nbody-n-4-g-4_2 && \
+make nbody-n-4-g-4_3 && \
+cd ${source_folder} && \
+
+sbatch job.sh
