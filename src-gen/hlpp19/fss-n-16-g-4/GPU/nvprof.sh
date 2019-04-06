ed08e03387ce0d15fc53a08e9aa63fbe9a72e351
Fabian Wrede <fabian.wrede@wi.uni-muenster.de>
2019-04-06 21:42:24 +0200

    re-gen

--- src-gen/hlpp19/fss-n-16-g-4/GPU/nvprof.sh
+++ src-gen/hlpp19/fss-n-16-g-4/GPU/nvprof.sh
@@ -4,11 +4,11 @@ source_folder=${PWD} && \
 
 # remove files and create folder
 mkdir -p /home/fwrede/musket-build/hlpp19/fss-n-16-g-4/GPU/out/ && \
-rm -rf -- /home/fwrede/musket-build/hlpp19/fss-n-16-g-4/GPU/build/nvprof && \
-mkdir -p /home/fwrede/musket-build/hlpp19/fss-n-16-g-4/GPU/build/nvprof && \
+rm -rf -- ~/build/mnp/fss-n-16-g-4/gpu && \
+mkdir -p ~/build/mnp/fss-n-16-g-4/gpu && \
 
 # run cmake
-cd /home/fwrede/musket-build/hlpp19/fss-n-16-g-4/GPU/build/nvprof && \
+cd ~/build/mnp/fss-n-16-g-4/gpu && \
 cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Nvprof -D CMAKE_CXX_COMPILER=pgc++ ${source_folder} && \
 
 make fss-n-16-g-4_0 && \
--- src-gen/hlpp19/nbody-n-16-g-2/GPU/nvprof.sh
+++ src-gen/hlpp19/nbody-n-16-g-2/GPU/nvprof.sh
@@ -4,11 +4,11 @@ source_folder=${PWD} && \
 
 # remove files and create folder
 mkdir -p /home/fwrede/musket-build/hlpp19/nbody-n-16-g-2/GPU/out/ && \
-rm -rf -- /home/fwrede/musket-build/hlpp19/nbody-n-16-g-2/GPU/build/nvprof && \
-mkdir -p /home/fwrede/musket-build/hlpp19/nbody-n-16-g-2/GPU/build/nvprof && \
+rm -rf -- ~/build/mnp/nbody-n-16-g-2/gpu && \
+mkdir -p ~/build/mnp/nbody-n-16-g-2/gpu && \
 
 # run cmake
-cd /home/fwrede/musket-build/hlpp19/nbody-n-16-g-2/GPU/build/nvprof && \
+cd ~/build/mnp/nbody-n-16-g-2/gpu && \
 cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Nvprof -D CMAKE_CXX_COMPILER=pgc++ ${source_folder} && \
 
 make nbody-n-16-g-2_0 && \
