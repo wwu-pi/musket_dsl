377f249c0f22da980303e165c18225ba6473c3ef
Fabian Wrede <fabian.wrede@wi.uni-muenster.de>
2019-04-06 22:33:56 +0200

    re-gen

--- src-gen/hlpp19/11-build-and-submit-cuda-nvprof.sh
+++ src-gen/hlpp19/11-build-and-submit-cuda-nvprof.sh
@@ -6,7 +6,7 @@ current_folder=${PWD} && \
 for benchmark in frobenius matmult nbody; do
   for node in 1 4 16; do
     for gpu in 1 2 4; do
-      DIR="${benchmark}-n-${node}-g-${gpu}/GPU"
+      DIR="${benchmark}-n-${node}-g-${gpu}/CUDA"
       if [ -d "${DIR}" ]; then
         cd ${DIR} && \
     	  chmod +x nvprof.sh && \
