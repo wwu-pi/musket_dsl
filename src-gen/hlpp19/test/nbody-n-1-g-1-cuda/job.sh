cee20b11a4a171fa467e13d7f9eea5adbc4ab9a3
Fabian Wrede <fabian.wrede@wi.uni-muenster.de>
2019-04-10 15:33:10 +0200

    hlpp: nbody test

--- src-gen/hlpp19/test/nbody-n-1-g-1-cuda/job.sh
+++ src-gen/hlpp19/test/nbody-n-1-g-1-cuda/job.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name nbody-n-1-g-1-GPU-nodes-1-gpu-1
+#SBATCH --job-name nbody-test-n-1-g-1-cuda
 #SBATCH --ntasks 1
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --partition gpu2
 #SBATCH --exclusive
-#SBATCH --output /home/fwrede/musket-build/hlpp19/nbody-n-1-g-1/CUDA/out/nbody-n-1-g-1-nodes-1-gpu-1.out
+#SBATCH --output /home/fwrede/out/hlpp19/musket-test/nbody-n-1-g-1-cuda.out
 #SBATCH --cpus-per-task 24
 #SBATCH --mail-type ALL
 #SBATCH --mail-user fabian.wrede@mailbox.tu-dresden.de
-#SBATCH --time 00:05:00
+#SBATCH --time 00:15:00
 #SBATCH -A p_algcpugpu
 #SBATCH --gres gpu:4
 
@@ -17,5 +17,5 @@ export OMP_NUM_THREADS=24
 
 RUNS=1
 for ((i=1;i<=RUNS;i++)); do
-    srun --multi-prog /home/fwrede/musket/src-gen/hlpp19/nbody-n-1-g-1/CUDA/job.conf
+    srun --multi-prog /home/fwrede/musket/src-gen/hlpp19/test/nbody-n-1-g-1-cuda/job.conf
 done
